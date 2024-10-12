8bdfe6dde6208930bd2c281755bacc87da48e0ec
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-10-12 18:35:22 -0400

    s

--- sentiment.py
+++ sentiment.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
 
-col = client['restaurants']['reviews']
+col = client['test']['reviews']
 
 with open("reviews.csv", 'r', encoding='utf-8', errors='ignore') as f:
     reviews = f.readlines()
@@ -18,9 +18,10 @@ with open("reviews.csv", 'r', encoding='utf-8', errors='ignore') as f:
         text = TextBlob(rev)
         polarity = text.sentiment.polarity
 
+
         document = {
             "review":rev,
-            "polarity":polarity
+            "polarity":polarity,
             }
         
         insert_result = col.insert_one(document)

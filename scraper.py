f1dcb70acb31ad1a1b968e0699912252b896c055
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-10-12 18:35:15 -0400

    Did sentiment analysics and added

--- scraper.py
+++ scraper.py
@@ -2,14 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-for idx in range(10,230):
-    url = f"https://www.yelp.com/biz/ozakaya-brooklyn?osq=Restaurants&start={idx}#reviews"
-    response = requests.get(url)
-    html_content = response.text
-    soup = BeautifulSoup(html_content,'html.parser')
-    reviews=soup.find_all("p",{"class":"comment__09f24__D0cxf y-css-h9c2fl"})
+restaurants = ['ozakaya-brooklyn','arden-brooklyn','the-canary-brooklyn']
 
-    with open('reviews.csv', 'a', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        for review in reviews:
-            csvwriter.writerow(review.text)
+for restaurant in restaurants:
+    for idx in range(30):
+        url = f"https://www.yelp.com/biz/{restaurant}start={idx}#reviews"
+        response = requests.get(url)
+        html_content = response.text
+        soup = BeautifulSoup(html_content,'html.parser')
+        reviews=soup.find_all("p",{"class":"comment__09f24__D0cxf y-css-h9c2fl"})
+        print(reviews)
+        with open('reviews.csv', 'a', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=' ')
+            for review in reviews:
+                csvwriter.writerow(review.text)
